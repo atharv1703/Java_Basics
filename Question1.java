964f9d5183c3e15c7cf6b11e4425e527d5a7b2e3
Atharv Salpekar <53127015+atharv1703@users.noreply.github.com>
2021-02-02 18:28:18 +0530

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,8 @@
+public class Question1 {
+    public static void main(String[] args) {
+        System.out.println("Name:   Atharv Salpekar");
+        System.out.println("Roll:   1905668");
+        System.out.println("Class:  CS10");
+        System.out.println("Branch: CSE");
+    }
+}
