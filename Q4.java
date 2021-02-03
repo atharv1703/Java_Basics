154e06a5af513a344c06fab36d3ab075d650165d
Atharv Salpekar <53127015+atharv1703@users.noreply.github.com>
2021-02-03 23:59:31 +0530

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Q4 {
+
+    static int count=0;
+
+    Q4(){
+        count++;   
+    }
+
+    public static void main(String[] args) {
+
+        Q4 obj1 = new Q4();
+        Q4 obj2 = new Q4();
+        Q4 obj3 = new Q4();
+        Q4 obj4 = new Q4();
+        Q4 obj5 = new Q4();
+        Q4 obj6 = new Q4();
+        Q4 obj7 = new Q4();
+        
+        System.out.println("Number of Objects: "+ count);
+    }
+    
+}
